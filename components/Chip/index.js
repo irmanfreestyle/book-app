a096413db8b509fb259c330c084d2697e320c586
irmanfreestyle <=>
2022-04-27 07:03:04 +0700

    first setup

---
+++ components/Chip/index.js
@@ -0,0 +1,12 @@
+import styles from './_chip.module.scss'
+
+export default function Chip({ text, icon }) {
+  return (
+    <div className={ styles.chip }>
+      <div className='d-flex align-items-center gap-1'>
+        { icon && <i className='material-icons text-primary' style={{fontSize: '15px'}}>adb</i> }
+        { text }
+      </div>
+    </div>
+  )
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const https = require('https');
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  https.get('https://asia-southeast2-sejutacita-app.cloudfunctions.net/fee-assessment-categories', (result) => {
+    result.on('data', (d) => {
+      res.status(200).json(d)
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });
 }
